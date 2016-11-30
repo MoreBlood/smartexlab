571edd7bede43d773e511106af09fac710d3886f
MoreBlood <new.shtirlic@mail.ru>
2016-11-30 03:28:49 +0300

    Some Tweaks
    - added open animation
    - exception catcher
    - title for different states of window
    - now state saves by username tag

--- js/list.js
+++ js/list.js
@@ -4,13 +4,14 @@ var workerlistapp = angular.module("workerlistapp", []);
     	$http.get('http://jsonplaceholder.typicode.com/users').then(function(responce){ //get JSON file
 
             $scope.Data = responce.data;
-            currentPos = $location.url().replace("/", "");            
-            if (!(currentPos == ""))  $scope.ShowCurrent($scope.Data[$scope.FindIndex($scope.Data, "id", currentPos)]);                  
+            currentPos = $location.url();            
+            if (currentPos != "/")  $scope.ShowCurrent($scope.Data[$scope.FindIndex($scope.Data, "username", currentPos.replace("/", ""))]);                  
         });
 
         $scope.FindIndex = function (object, key, value) { //function for search by a key
 
-            for (var i = 0; i < object.length; i++) {             
+            for (var i in object) {
+
                 if (object[i][key] == value)
                 return i;                
             }
@@ -21,24 +22,34 @@ var workerlistapp = angular.module("workerlistapp", []);
         pop_text = angular.element(document.querySelector(".modal-body")),
         pop_title = angular.element(document.querySelector(".modal-title"));
       
-        $scope.ShowCurrent = function (element){            
-            
-            pop.css("display", "block");
-            pop_title.html(element.name);
-            pop_text.html( "<b>Website: </b>" + '<a href ="http://'+ element.website + '">' + element.website + "</a>" + "<br>"+
-                            "<b>Phone: </b>" + element.phone + "<br>"+
-                            "<b>Company: </b>" +  element.company.name + "<br>"+
-                            "<i>" + element.company.catchPhrase + "<br>"+
-                            element.company.bs + "</i><br>"+ "<b>Address:</b> <br> <i>"+
-                            element.address.street + " "+ element.address.suite + "<br>"+
-                            element.address.city + " " + element.address.zipcode + "</i>"
-                            );
-            $location.url(element.id); // set url according to the open modal window
+        $scope.ShowCurrent = function (element){
+
+            try{
+
+                pop.addClass("visible-modal");
+                pop_title.html(element.name);
+                pop_text.html( "<b>Website: </b>" + '<a href ="http://'+ element.website + '">' + element.website + "</a>" + "<br>"+
+                                "<b>Phone: </b>" + element.phone + "<br>"+
+                                "<b>Company: </b>" +  element.company.name + "<br>"+
+                                "<i>" + element.company.catchPhrase + "<br>"+
+                                element.company.bs + "</i><br>"+ "<b>Address:</b> <br> <i>"+
+                                element.address.street + " "+ element.address.suite + "<br>"+
+                                element.address.city + " " + element.address.zipcode + "</i>"
+                                );
+                $location.url(element.username); // set url according to the open modal window
+                document.title = "Worker - " + element.name;
+            }
+            catch(e){
+
+                if (e.name == "TypeError") pop_text.html("No such data...");
+                else console.log(e);
+            }
         }
 
         $scope.hide = function (){
-            
-            pop.css("display", "none");
+
+            pop.removeClass("visible-modal");
             $location.url(""); // clear url if closed
+            document.title = "Workers";
         };
 });
